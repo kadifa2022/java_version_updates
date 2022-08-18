3f591f7169dbbc68f1259f63f78da5f9862c7f25
Kadifa <Kudickadifa@gmail.com>
2022-08-17 19:56:13 -0500

    Functional interface practice

--- m3-lambda/src/main/java/com/cydeo/FunctionalInterface.java
+++ m3-lambda/src/main/java/com/cydeo/FunctionalInterface.java
@@ -1,6 +1,8 @@
 package com.cydeo;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class FunctionalInterface {// functional Interface ready interfaces -created -parameter and return type
@@ -26,9 +28,19 @@ public class FunctionalInterface {// functional Interface ready interfaces -crea
 
         // CONSUMER ACCEPTS OBJECT, DOES NOT RETURN ANYTHING
 
-        Consumer<Integer> display= i-> System.out.println();
+        Consumer<Integer> display= i-> System.out.println(i);
         display.accept(10);
 
+        System.out.println("******************BI CONSUMER**********************");
+        //BI CONSUMER ACCEPTING 2 OBJECTS AND RETURN NOTHING
+
+        BiConsumer<Integer, Integer> addTwo= (x,y) -> System.out.println(x+y);
+        addTwo.accept(1,2);
+
+        System.out.println("******************FUNCTION**********************");
+        //ACCEPTING OBJECT AND RETURNS OBJECT <>Generic
+        Function<String, String> fun= s-> "Hello" + s;
+        System.out.println(fun.apply("Cydeo")); //if returning something we need sout
 
 
 
