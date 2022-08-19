51eb3b9ef19d40b76beb6ef2b72f31b083b894b5
Kadifa <Kudickadifa@gmail.com>
2022-08-19 08:45:16 -0500

    Consumer practice

--- m3-lambda/src/main/java/com/cydeo/TaskSelfPractice/ConsumerPractice2.java
+++ m3-lambda/src/main/java/com/cydeo/TaskSelfPractice/ConsumerPractice2.java
@@ -1,8 +1,21 @@
 package com.cydeo.TaskSelfPractice;
 
+import java.util.function.Consumer;
+
 public class ConsumerPractice2 {
 
     public static void main(String[] args) {
+
+
+        Consumer<int[]> printEach = n -> {
+            for (int each: n) {
+                System.out.println(each);
+                
+            }
+        };
+        int [] numbers ={100,200,300,400};
+        printEach.accept(numbers);
+
 
     }
 }
