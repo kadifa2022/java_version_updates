980285c4363756c114a130a6f58902e5c2a437b7
Kadifa <Kudickadifa@gmail.com>
2022-08-22 20:48:48 -0500

    Collectors

--- m5-streem/src/main/java/com/cydeo/CollectorsDemo.java
+++ m5-streem/src/main/java/com/cydeo/CollectorsDemo.java
@@ -2,6 +2,7 @@ package com.cydeo;
 
 import com.cydeo.task.Dish;
 import com.cydeo.task.DishData;
+import com.cydeo.task.Type;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -63,6 +64,17 @@ public class CollectorsDemo {
         String str = courses.stream()
                 .collect(Collectors.joining(","));
         System.out.println(str);
+        //partitioningBy() : is used to partition a stream of objects (or set of elements) based on given predicate)
+
+        Map<Boolean,List<Dish>>veggieDish=DishData.getAll().stream()
+                .collect(Collectors.partitioningBy(Dish::isVegetarian));
+        System.out.println(veggieDish);
+
+        //groupingBy() : is used for grouping objects by some property and storing results in a Map Instance
+
+        Map<Type,List<Dish>>dishType=DishData.getAll().stream()
+                .collect(Collectors.groupingBy(Dish::getType));
+        System.out.println(dishType);
 
 
 
