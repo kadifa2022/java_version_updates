ef413ae00dc62e4b0f942090a172d2903b3c218c
Kadifa <Kudickadifa@gmail.com>
2022-08-25 17:23:35 -0500

    Stream  practice

--- m5-streem/src/main/java/com/cydeo/selfPractice/StreamOperations.java
+++ m5-streem/src/main/java/com/cydeo/selfPractice/StreamOperations.java
@@ -7,7 +7,7 @@ public class StreamOperations {
 
     public static void main(String[] args) {
 
-        List<Integer> list = Arrays.asList(1,2,3,4,5);
+        List<Integer> list = Arrays.asList(1,2,2,3,4,4,5);
         list.forEach(System.out::println);
 
         System.out.println("----------Filter---------------");
@@ -16,6 +16,33 @@ public class StreamOperations {
         list.stream()
                 .filter(i -> i%2==0)
                 .forEach(System.out::println);
+
+        System.out.println("-----------Distinct--------------------");
+        list.stream()
+                .filter(i->i%2==0)
+                .distinct()
+                .forEach(System.out::println);
+
+
+        System.out.println("---------------Limit(n)----------------------");
+
+        list.stream()
+                .filter(i->i%2==0)
+                .limit(4)
+                .forEach(System.out::println);
+
+        System.out.println("---------------Skip(1)------------------------");
+        list.stream()
+                .filter(i->i%2==0)
+                .skip(1)
+                .forEach(System.out::println);
+
+
+
+
+
+
+
 
     }
 }
