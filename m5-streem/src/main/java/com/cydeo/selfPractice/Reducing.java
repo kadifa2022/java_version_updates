68d1c5406ea2ca5c5468f6851033a8a0674a47cf
Kadifa <Kudickadifa@gmail.com>
2022-08-26 06:48:41 -0500

    Terminal operations  practice

--- m5-streem/src/main/java/com/cydeo/selfPractice/Reducing.java
+++ m5-streem/src/main/java/com/cydeo/selfPractice/Reducing.java
@@ -2,21 +2,47 @@ package com.cydeo.selfPractice;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class Reducing {
 
     public static void main(String[] args) {
 
         List<Integer> numbers= Arrays.asList(4,5,7,8,8);
-        int sum=0;
+       /*
+       int sum=0;
 
-            for(int number :numbers){
-
-                sum=sum+number;
+        for(int number :numbers){
+          sum=sum+number;
             }
-            int result=numbers.stream().reduce(0,(a,b)->a+b);
+            */
+           int result=numbers.stream().reduce(0,(a,b)->a+b);
 
-        System.out.println(result);
+    System.out.println(result);
+
+        System.out.println("-----------Dish Calories Total---------");
+
+        Optional<Integer> calTotal=DishData.getAll().stream()
+                .map(Dish::getCalories)
+                .reduce((a,b) ->(a+b));
+                                                  //getClass from Class
+        System.out.println(calTotal.get());        //.get() with Optional
+
+        // MIN AND MAX METHODE
+
+        Optional<Integer> min=numbers.stream().reduce(Integer::min);
+        Optional<Integer> max =numbers.stream().reduce(Integer::max);
+        Optional<Integer> sum=numbers.stream().reduce(Integer::sum);
+
+        System.out.println("Min "+ min.get());
+        System.out.println("Max "+ max.get());
+        System.out.println("Sum "+ sum.get());
+
+
+
+
+
+
 
 
     }
