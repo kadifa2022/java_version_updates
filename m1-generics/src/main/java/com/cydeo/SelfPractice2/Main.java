17b09f746f52af6cb474eaf041edc5d19397df19
Kadifa <Kudickadifa@gmail.com>
2023-05-05 13:00:25 -0500

    generics task

---
+++ m1-generics/src/main/java/com/cydeo/SelfPractice2/Main.java
@@ -0,0 +1,30 @@
+package main.java.com.cydeo.SelfPractice2;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Array list only accepts objects belong to this class
+        ArrayList<Integer> items = new ArrayList<Integer>();// genetic< class name objects>  come with version Java 5
+
+        items.add(1);
+        items.add(2);
+        items.add(3);
+        items.add(4);
+        //items.add("Apple");
+
+
+        printDoubled(items);
+    }
+    private static void printDoubled(ArrayList<Integer> items){
+        for (Integer i : items) {
+            System.out.println(i * 2);
+        }
+        System.out.println(items);
+    }
+
+
+
+
+}
