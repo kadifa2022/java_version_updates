5cdc91a0659f9f007303728e0a5809bd7c72704e
Kadifa <Kudickadifa@gmail.com>
2023-05-06 09:02:22 -0500

    Behavior parameterization

---
+++ m3-lambda/src/main/java/com/cydeo/TaskSelfPractice/appleTask/AppleTest.java
@@ -0,0 +1,15 @@
+package com.cydeo.TaskSelfPractice.appleTask;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppleTest {
+
+    public static void main(String[] args) {
+
+        List<Apple> inventory = new ArrayList<>();
+
+
+
+    }
+}
---
+++ m3-lambda/src/main/java/com/cydeo/TaskSelfPractice/appleTask/Color.java
@@ -0,0 +1,6 @@
+package com.cydeo.TaskSelfPractice.appleTask;
+
+public enum Color {
+
+    GREEN, RED
+}
