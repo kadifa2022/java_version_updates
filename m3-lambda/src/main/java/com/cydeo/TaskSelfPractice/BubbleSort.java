144800bfd98ac2368071eab9d63570630516f106
Kadifa <Kudickadifa@gmail.com>
2023-05-05 21:12:31 -0500

    Behavior parameterization

---
+++ m3-lambda/src/main/java/com/cydeo/TaskSelfPractice/BubbleSort.java
@@ -0,0 +1,11 @@
+package com.cydeo.TaskSelfPractice;
+
+public class BubbleSort implements Sorting{
+
+
+    @Override
+    public void sort() {
+        System.out.println("Bubble Sorting");
+
+    }
+}
