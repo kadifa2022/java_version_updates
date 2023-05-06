ebf53e78f730346a3addcae6a126bbfd04c34370
Kadifa <Kudickadifa@gmail.com>
2023-05-06 18:52:50 -0500

    Lambda practice

--- m3-lambda/src/main/java/com/cydeo/TaskSelfPractice/appleTask/AppleTest.java
+++ m3-lambda/src/main/java/com/cydeo/TaskSelfPractice/appleTask/AppleTest.java
@@ -7,9 +7,45 @@ public class AppleTest {
 
     public static void main(String[] args) {
 
+        //apple object
         List<Apple> inventory = new ArrayList<>();
+
+        inventory.add(new Apple(300,Color.RED));
+        inventory.add(new Apple(200,Color.GREEN));
+        inventory.add(new Apple(140,Color.GREEN));
+        inventory.add(new Apple(100,Color.RED));
+        inventory.add(new Apple(130,Color.GREEN));
+
+
+        List<Apple> heavyApple= filterApples(inventory, new AppleHeavyPredicate());// looking for new  object
+        System.out.println(heavyApple);
+        List <Apple> greenApple= filterApples(inventory, new AppleGreenColorPredicate());
+        System.out.println(greenApple);
+                                   //TypeInference
+        ApplePredicate weightApple= (Apple apple)->apple.getWeight()>200;
+        filterApples(inventory, weightApple);
 
 
 
     }
+
+
+
+
+    // Strategy   Design Patter for application
+    //1. CREATE INTERFACE(abstract methode) 2. CREATE CLASSES TO IMPLEMENT INTERFACE
+    //3. create method to (accept a bunch of behaviors) filter all apples and using applePredicate Interface(for different behavior)Polymorphism
+    private static List<Apple> filterApples(List<Apple> inventory, ApplePredicate applePredicate){// to accept different behaviors GREEN/HEAVY>200
+        List<Apple> result = new ArrayList<>();// empty bucket for my returning
+
+
+        for (Apple apple : inventory) {// checking each apple
+            if(applePredicate.testApple(apple)){
+                result.add(apple);
+            }
+        }
+
+        return result;
+    }
+
 }
