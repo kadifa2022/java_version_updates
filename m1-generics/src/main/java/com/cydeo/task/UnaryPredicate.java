fe1e5703cac3a0d80d34c11349b11ccc3f58f305
Kadifa <Kudickadifa@gmail.com>
2023-05-05 20:37:23 -0500

    enums task

--- m1-generics/src/main/java/com/cydeo/task/UnaryPredicate.java
+++ m1-generics/src/main/java/com/cydeo/task/UnaryPredicate.java
@@ -1,5 +1,5 @@
 package main.java.com.cydeo.task;
 
 public interface UnaryPredicate<T> {
-public boolean test(T obj);
+public boolean test(T obj);// abstract method
 }
---
+++ m1-generics/src/main/java/com/cydeo/task2/UnaryPredicate.java
@@ -0,0 +1,6 @@
+package main.java.com.cydeo.task2;
+
+public interface UnaryPredicate <T>{
+
+    public boolean test (T obj);
+}
