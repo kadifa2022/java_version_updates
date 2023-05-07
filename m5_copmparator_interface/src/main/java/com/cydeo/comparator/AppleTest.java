fdf25be98a0321db2d6e97d096b59284d6d4aab7
Kadifa <Kudickadifa@gmail.com>
2023-05-07 16:38:22 -0500

    Comparator interface practice

--- m5_copmparator_interface/src/main/java/com/cydeo/comparator/AppleTest.java
+++ m5_copmparator_interface/src/main/java/com/cydeo/comparator/AppleTest.java
@@ -1,7 +1,10 @@
 package com.cydeo.comparator;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+
+import static java.util.Comparator.comparing; // to import static method
 
 public class AppleTest {
 
@@ -16,6 +19,25 @@ public class AppleTest {
         inventory.add(new Apple(200, Color.GREEN));
         inventory.add(new Apple(150, Color.GREEN));
         inventory.add(new Apple(50, Color.RED));
+
+        Comparator<Apple> sortApple= comparing((Apple apple)-> apple.getWeight());
+        inventory.sort(sortApple);
+
+        System.out.println(inventory);
+
+        // importing static method // instead of calling through class name
+        //Comparator <Apple> sortApple2  = comparing (Apple::getWeight);// class name and method
+        inventory.sort(comparing (Apple::getWeight)); // simplify with ::
+        System.out.println(inventory);
+        // Reversed
+        inventory.sort(comparing (Apple::getWeight).reversed()); // descending order
+        System.out.println(inventory);
+
+        // Channing methods
+        inventory
+                .sort(comparing(Apple::getWeight)
+                .reversed().thenComparing(Apple::getColor));
+
 
 
     }
