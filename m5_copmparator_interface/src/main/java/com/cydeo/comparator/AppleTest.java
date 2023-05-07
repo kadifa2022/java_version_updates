ea82c62f9c04ccd0b35f8ccd493b3699fb87d493
Kadifa <Kudickadifa@gmail.com>
2023-05-07 16:03:05 -0500

    Comparator interface practice

---
+++ m5_copmparator_interface/src/main/java/com/cydeo/comparator/AppleTest.java
@@ -0,0 +1,23 @@
+package com.cydeo.comparator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppleTest {
+
+    public static void main(String[] args) {
+
+
+        List<Apple> inventory = new ArrayList<>();
+
+
+        inventory.add(new Apple(300, Color.GREEN));
+        inventory.add(new Apple(100, Color.RED));
+        inventory.add(new Apple(200, Color.GREEN));
+        inventory.add(new Apple(150, Color.GREEN));
+        inventory.add(new Apple(50, Color.RED));
+
+
+    }
+
+}
